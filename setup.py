b195550bcc9c6a0065c03467bb45352d46a8a125
Ty <ty.cumby@gmail.com>
2021-11-07 11:28:17 -0700

    Replace the setup.py contents to the setup_agpl.py contents

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ with open("README.md", "r") as fd:
     long_description = fd.read()
 
 setuptools.setup(
-    name="miss_hit_core",
+    name="miss_hit",
     version=miss_hit_core.version.VERSION,
     author="Florian Schanda",
     author_email="florian@schanda.org.uk",
-    description="Code formatting and code metrics for programs written in the MATLAB/Simulink and Octave languages.",
+    description="Static analysis and other utilities for programs written in the MATLAB/Simulink and Octave languages.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://misshit.org",
@@ -22,16 +22,14 @@ setuptools.setup(
         "Documentation" : "https://florianschanda.github.io/miss_hit/",
         "Source Code"   : "https://github.com/florianschanda/miss_hit",
     },
-    license="GNU General Public License v3",
-    packages=["miss_hit_core"],
+    license="GNU Affero General Public License v3",
+    packages=["miss_hit"],
+    install_requires=["miss_hit_core==%s" % miss_hit_core.version.VERSION],
     python_requires=">=3.6, <4",
-    package_data = {
-        "miss_hit_core": ["resources/*.css", "resources/assets/*"],
-    },
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Topic :: Scientific/Engineering",
@@ -41,8 +39,9 @@ setuptools.setup(
     ],
     entry_points={
         "console_scripts": [
-            "mh_style = miss_hit_core.mh_style:main",
-            "mh_metric = miss_hit_core.mh_metric:main",
+            "mh_lint = miss_hit.mh_lint:main",
+            "mh_diff = miss_hit.mh_diff:main",
+            "mh_copyright = miss_hit.mh_copyright:main",
         ],
     },
 )
